b9bca58954b29a8a4e37c252fa9a84a0d9df25bb
Youngislander <yjmnopainnogain@gmail.com>
2020-07-30 17:27:05 +0900

    Apollo context & ScrollView

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView, RefreshControl } from "react-native";
 import styled from "styled-components/native";
+import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
+import { useQuery } from "react-apollo-hooks";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Text = styled.Text``;
 
 const View = styled.View`
   justify-content: center;
@@ -8,10 +43,24 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
-
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const { loading, data, refetch } = useQuery(FEED_QUERY);
+  const refresh = async () => {
+    try {
+      setRefreshing(true);
+      await refetch();
+    } catch(e){
+      console.log(e);
+    } finally {
+      setRefreshing(false);
+    }
+  };
+  return (
+    <ScrollView
+     refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}
+    >
+      {loading ? <Loader /> : <Text>Hello</Text>}
+    </ScrollView>
+  );
+};
